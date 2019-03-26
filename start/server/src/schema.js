def9bf2a950713e876681afbf9f3677375860778
Dmytro Borysov <d.borysov@tmg.nl>
2019-03-26 13:16:11 +0100

    feat(server): add initial models

---
+++ start/server/src/schema.js
@@ -0,0 +1,56 @@
+const { gql } = require('apollo-server')
+
+module.exports = gql`
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+
+  type Mutation {
+    # if false, booking trips failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+
+    # if false, cancellation failed -- check errors
+    cancelTrips(launchId: ID!): TripUpdateResponse!
+
+    login(email: String!): String # Login token
+  }
+
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    # Queries for the current user
+    me: User
+  }
+`
