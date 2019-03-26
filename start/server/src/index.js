15bc53d7ab8db9994f8abd4a07ffa06bc552ec70
Dmytro Borysov <d.borysov@tmg.nl>
2019-03-26 15:26:47 +0100

    feat(server): add data sources

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,5 +1,16 @@
 const { ApolloServer } = require('apollo-server')
+const { createStore } = require('./utils')
 const typeDefs = require('./schema')
+const LaunchAPI = require('./datasources/launch')
+const UserAPI = require('./datasources/user')
 
-const server = new ApolloServer({typeDefs})
-server.listen().then(({url}) => console.log(`🚀 Server ready at ${url}`))
+const store = createStore()
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+})
+server.listen().then(({ url }) => console.log(`🚀 Server ready at ${url}`))
