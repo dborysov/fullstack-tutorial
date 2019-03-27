0636771baa1c1f34428d5348418703a61d71b780
Dmytro Borysov <d.borysov@tmg.nl>
2019-03-27 10:34:15 +0100

    feat(client): manually fetching data

---
+++ start/client/src/index.js
@@ -0,0 +1,24 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import gql from 'graphql-tag'
+
+const cache = new InMemoryCache()
+const link = new HttpLink({ uri: 'https://fullstack-tutorial-server-bpmxvhumbk.now.sh/' })
+
+const client = new ApolloClient({ cache, link })
+
+client
+  .query({
+    query: gql`
+      query GetLaunch {
+        launch(id: 56) {
+          id
+          mission {
+            name
+          }
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result))
